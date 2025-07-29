50f78dcdc41d0fc88c19af5b789de8f9339c8828
Eau-Tarie <eaux-taries@pm.me>
2025-07-29 15:17:45 +0200

    waybar - add custom, group and system modules. Set emplacements

--- waybar/scripts/clock.sh
+++ waybar/scripts/clock.sh
@@ -3,13 +3,13 @@
 HOUR=$(date +%H)
 
 if (( 6 <= HOUR && HOUR < 10 )); then
-    ICON=""  # Matin
+    ICON="󰖜"  # Matin
 elif (( 10 <= HOUR && HOUR < 18 )); then
     ICON=""  # Après-midi
 elif (( 18 <= HOUR && HOUR < 22 )); then
-    ICON=""  # Soir
+    ICON="󰖛"  # Soir
 else
-    ICON=""  # Nuit
+    ICON=""  # Nuit
 fi
 
 TIME=$(date "+%H:%M:%S")
