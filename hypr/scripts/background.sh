4c556e975144f08baeaeae78028a5b046689f55d
Eau-Tarie <eaux-taries@pm.me>
2025-07-29 17:24:57 +0200

    add script to change color and background denpending of time

---
+++ hypr/scripts/background.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Dossiers de wallpapers classés par moment de la journée
+WALL_DIR="$HOME/assets/wallpapers"
+
+HOUR=$(date +%H)
+
+if (( HOUR >= 6 && HOUR < 10 )); then
+    SUBDIR="sunrise"
+elif (( HOUR >= 10 && HOUR < 18 )); then
+    SUBDIR="day"
+elif (( HOUR >= 18 && HOUR < 22 )); then
+    SUBDIR="sunset"
+else
+    SUBDIR="night"
+fi
+
+TARGET_DIR="$WALL_DIR/$SUBDIR"
+
+IMAGE=$(find "$TARGET_DIR" -type f \( -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' \) | shuf -n 1)
+
+cp "$IMAGE" "$HOME/assets/wallpapers/current-background.png"
---
+++ hypr/scripts/hours.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HOUR=$(date +%H)
+
+if (( HOUR >= 6 && HOUR < 10 )); then
+    COLOR="#FFA500"
+elif (( HOUR >= 10 && HOUR < 18 )); then
+    COLOR="#00BFFF"
+elif (( HOUR >= 18 && HOUR < 22 )); then
+    COLOR="#FF4500"
+else
+    COLOR="#708090"
+fi
+
+echo "<span foreground=\"$COLOR\"><b>$HOUR</b></span>"
