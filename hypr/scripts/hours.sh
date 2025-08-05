d54aaf2337a50b1059c02aa01bde4cd83d530330
Eau-Tarie <eaux-taries@pm.me>
2025-08-05 08:49:13 +0200

    change hyprlock display and color

--- hypr/scripts/hours.sh
+++ hypr/scripts/hours.sh
@@ -3,13 +3,13 @@
 HOURFORMATTED=$((10#$(date +%H)))
 HOUR=$(date +%H)
 if (( HOURFORMATTED >= 6 && HOURFORMATTED < 10 )); then
-    COLOR="#FFA500"
+    COLOR="#FF66CC"
 elif (( HOURFORMATTED >= 10 && HOURFORMATTED < 18 )); then
-    COLOR="#00BFFF"
-elif (( HOURFORMATTED >= 18 && HOURFORMATTED < 22 )); then
     COLOR="#FF4500"
+elif (( HOURFORMATTED >= 18 && HOURFORMATTED < 22 )); then
+    COLOR="#D4E2F0"
 else
-    COLOR="#708090"
+    COLOR="#667EFC"
 fi
 
 echo "<span foreground=\"$COLOR\"><b>$HOUR</b></span>"
--- hypr/scripts/weather.sh
+++ hypr/scripts/weather.sh
@@ -4,16 +4,16 @@ HOUR=$((10#$(date +%H)))
 
 if (( HOUR >= 6 && HOUR < 10 )); then
 	ICON="󰖜"
-	COLOR="#FFA500"
+	COLOR="#FF66CC"
 elif (( HOUR >= 10 && HOUR < 18 )); then
 	ICON=""
-	COLOR="#00BFFF"
+	COLOR="#FF4500"
 elif (( HOUR >= 18 && HOUR < 22 )); then
 	ICON="󰖛"
-	COLOR="FF4500"
+	COLOR="#D4E2F0"
 else
 	ICON=""
-	COLOR="#708090"
+	COLOR="#667EFC"
 fi
 
 echo "<span foreground=\"$COLOR\"><b>$ICON </b></span>"
